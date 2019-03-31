50257e2e21619079b74db2bd31a95796b3766908
rsr2425 <rsr2425@gmail.com>
2019-03-31 14:33:53 -0400

    Initial tests complete.

---
+++ organize.py
@@ -0,0 +1,44 @@
+'''
+Module: Organize
+
+This is the main file of the program "Clean" which should be used to call
+the tool from the command line.
+
+NOTE: Although the final form of this application will actually move the
+files into folders based on the categories (and eventually hierarchy)
+developed eventually, for the time being it just prints out the proposed
+structure.
+'''
+
+import os
+import sys
+import spacy
+
+from sklearn.preprocessing import normalize
+from sklearn.cluster import KMeans
+
+# load up spacy and ready it for use
+nlp = spacy.load('en_core_web_lg')
+
+# load directory intended for processing
+dir = sys.argv[1]
+
+# scan over all files and create doc2vec representation
+docs = {}
+for file in os.listdir(dir):
+     filename = os.fsdecode(file)
+     f =  open(dir + '/' + file)
+     text = f.read()
+     docs[filename] = nlp(text).vector
+     docs[filename] = normalize(docs[filename])
+n = len(docs)
+
+
+# determine number of clusters
+X = list(docs.values())
+n_clusters = 3
+
+kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
+print(list(zip(docs.keys(), kmeans.labels_)))
+
+# create a label for each cluster
